729062f8bb4d287e741221457e4af4404aa9719d
brianwithyou <brianwithyou@163.com>
2023-03-05 23:48:40 +0800

    反转链表 reverse singly linked list.

---
+++ src/com/brian/leet06ReverseLinkedList/Solution.java
@@ -0,0 +1,63 @@
+package com.brian.leet06ReverseLinkedList;
+
+/**
+ * @author : brian
+ * @since 0.1
+ */
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+
+            pre = cur;
+            cur = next;
+
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        int[] data = new int[] {1, 2, 3, 4, 5};
+
+        ListNode head = new ListNode(data[0]);
+        head.next = new ListNode(data[1]);
+        head.next.next = new ListNode(data[2]);
+        head.next.next.next = new ListNode(data[3]);
+        head.next.next.next.next = new ListNode(data[4]);
+
+//        while (head != null) {
+//            System.out.printf(head.val + ", ");
+//            head = head.next;
+//        }
+
+
+        ListNode temp = new Solution().reverseList(head);
+
+        while (temp != null) {
+            System.out.printf(temp.val + ", ");
+            temp = temp.next;
+        }
+
+    }
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
