fa931d4534ef454887ebf7df008ba0f67d0804a3
brianwithyou <brianwithyou@163.com>
2023-03-06 12:19:45 +0800

    25. k个一组反转链表[hard] 215.找出第k大的元素[easy]

---
+++ src/com/brian/leet25ReverseNodesKGroup/Solution.java
@@ -0,0 +1,74 @@
+package com.brian.leet25ReverseNodesKGroup;
+
+/**
+ * 给你链表的头节点 head ，每k个节点一组进行翻转，请你返回修改后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是k的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ *
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        // pre为待反转节点的前一个节点
+        ListNode pre = dummy;
+        // 待反转节点的最后一个节点
+        ListNode end = dummy;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode next = end.next;
+            // 断开
+            end.next = null;
+
+            // start节点只用于连接下一段，不能用end节点连接下一段，因为end节点是变化的
+            ListNode start = pre.next;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
