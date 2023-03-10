47b9a8431dd16fe3af4c5bab6095fad58f2ee491
brianwithyou <brianwithyou@163.com>
2023-03-10 23:50:47 +0800

    二叉树的层序遍历

---
+++ src/com/brian/leet102BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,53 @@
+package com.brian.leet102BinaryTreeLevelOrderTraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+/**
+ * 二叉树的层序遍历
+ * @author brian
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode poll = queue.poll();
+                level.add(poll.val);
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+
+            }
+            res.add(level);
+        }
+        return res;
+    }
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
