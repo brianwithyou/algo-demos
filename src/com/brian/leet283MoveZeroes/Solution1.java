25bce115bdfdbb8b3cd9b5c667eae33cb0400f7b
brianwithyou <brianwithyou@163.com>
2023-03-14 21:08:04 +0800

    数组：把0移动到数组最后面【easy】，方法2 原地覆盖，后面置为0

---
+++ src/com/brian/leet283MoveZeroes/Solution1.java
@@ -0,0 +1,29 @@
+package com.brian.leet283MoveZeroes;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution1 {
+
+    /**
+     * 原地覆盖，后面置为0
+     */
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
