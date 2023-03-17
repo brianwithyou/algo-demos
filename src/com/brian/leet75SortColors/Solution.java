b36afffb2931c6b93ab7704ef0bc9764a3b60589
brianwithyou <brianwithyou@163.com>
2023-03-17 23:18:42 +0800

    数组：给颜色排序，计数排序

---
+++ src/com/brian/leet75SortColors/Solution.java
@@ -0,0 +1,49 @@
+package com.brian.leet75SortColors;
+
+/**
+ * 给定一个包含红色、白色和蓝色、共n 个元素的数组nums，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ * 我们使用整数 0、1 和 2 分别表示红色、白色和蓝色。
+ * 必须在不使用库内置的 sort 函数的情况下解决这个问题。
+ *
+ * 输入：nums = [2,0,2,1,1,0]
+ * 输出：[0,0,1,1,2,2]
+ *
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution {
+
+    /**
+     * 计数排序
+     */
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count[0]++;
+            }
+            if (nums[i] == 1) {
+                count[1]++;
+            }
+            if (nums[i] == 2) {
+                count[2]++;
+            }
+        }
+
+        for (int i = 0; i < count[0]; i++) {
+            nums[i] = 0;
+        }
+        for (int i = count[0]; i < count[0] + count[1]; i++) {
+            nums[i] = 1;
+        }
+        for (int i = count[0] + count[1]; i < count[0] + count[1] + count[2]; i++) {
+            nums[i] = 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] tem = new int[] {2,0,2,1,1,0};
+        new Solution().sortColors(tem);
+    }
+}
