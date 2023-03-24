d6cd173482ab89a1134a6edb2e941a8fd0348550
brianwithyou <brianwithyou@163.com>
2023-03-24 22:50:27 +0800

    二叉树：二叉树的最小深度

---
+++ src/com/brian/leet111MinimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,43 @@
+package com.brian.leet111MinimumDepthOfBinaryTree;
+
+/**
+ * 给定一个二叉树，找出其最小深度。
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明：叶子节点是指没有子节点的节点。
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ *
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ *
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null)
+            return minDepth(root.right) + 1;
+        if (root.right == null)
+            return minDepth(root.left) + 1;
+
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
