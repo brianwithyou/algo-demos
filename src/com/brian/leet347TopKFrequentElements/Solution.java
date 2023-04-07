12c4d37365f486207b9cd6e08c837f00c7512ece
brianwithyou <brianwithyou@163.com>
2023-04-07 21:45:48 +0800

    优先队列：【347.返回其中出现频率前 k 高的元素】

---
+++ src/com/brian/leet347TopKFrequentElements/Solution.java
@@ -0,0 +1,41 @@
+package com.brian.leet347TopKFrequentElements;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        map.entrySet().forEach(entry -> {
+            queue.add(entry);
+        });
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = queue.poll().getKey();
+        }
+        return res;
+    }
+}
