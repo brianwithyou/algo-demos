3436f9d7d11ab867ab6d613abc82d83ef261e54e
brianwithyou <brianwithyou@163.com>
2023-04-11 20:05:45 +0800

    模拟：【1041.判断机器人是否困于环中】c/v

---
+++ src/com/brian/leet1041RobotBoundedInCircle/Solution.java
@@ -0,0 +1,82 @@
+package com.brian.leet1041RobotBoundedInCircle;
+
+/**
+ * 在无限的平面上，机器人最初位于(0, 0)处，面朝北方。注意:
+ *
+ * 北方向 是y轴的正方向。
+ * 南方向 是y轴的负方向。
+ * 东方向 是x轴的正方向。
+ * 西方向 是x轴的负方向。
+ * 机器人可以接受下列三条指令之一：
+ *
+ * "G"：直走 1 个单位
+ * "L"：左转 90 度
+ * "R"：右转 90 度
+ * 机器人按顺序执行指令instructions，并一直重复它们。
+ * 只有在平面中存在环使得机器人永远无法离开时，返回 true。否则，返回 false。
+ *
+ * 输入：instructions = "GGLLGG"
+ * 输出：true
+ * 解释：机器人最初在(0,0)处，面向北方。
+ * “G”:移动一步。位置:(0,1)方向:北。
+ * “G”:移动一步。位置:(0,2).方向:北。
+ * “L”:逆时针旋转90度。位置:(0,2).方向:西。
+ * “L”:逆时针旋转90度。位置:(0,2)方向:南。
+ * “G”:移动一步。位置:(0,1)方向:南。
+ * “G”:移动一步。位置:(0,0)方向:南。
+ * 重复指令，机器人进入循环:(0,0)——>(0,1)——>(0,2)——>(0,1)——>(0,0)。
+ * 在此基础上，我们返回true。
+ *
+ * 输入：instructions = "GG"
+ * 输出：false
+ * 解释：机器人最初在(0,0)处，面向北方。
+ * “G”:移动一步。位置:(0,1)方向:北。
+ * “G”:移动一步。位置:(0,2).方向:北。
+ * 重复这些指示，继续朝北前进，不会进入循环。
+ * 在此基础上，返回false。
+ *
+ * 输入：instructions = "GL"
+ * 输出：true
+ * 解释：机器人最初在(0,0)处，面向北方。
+ * “G”:移动一步。位置:(0,1)方向:北。
+ * “L”:逆时针旋转90度。位置:(0,1).方向:西。
+ * “G”:移动一步。位置:(- 1,1)方向:西。
+ * “L”:逆时针旋转90度。位置:(- 1,1)方向:南。
+ * “G”:移动一步。位置:(- 1,0)方向:南。
+ * “L”:逆时针旋转90度。位置:(- 1,0)方向:东方。
+ * “G”:移动一步。位置:(0,0)方向:东方。
+ * “L”:逆时针旋转90度。位置:(0,0)方向:北。
+ * 重复指令，机器人进入循环:(0,0)——>(0,1)——>(- 1,1)——>(- 1,0)——>(0,0)。
+ * 在此基础上，我们返回true。
+ *
+ * 链接：https://leetcode.cn/problems/robot-bounded-in-circle
+ *
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution {
+    /**
+     * 因此，机器人想要摆脱循环，在一串指令之后的状态，必须是不位于原点且方向朝北。
+     */
+    public boolean isRobotBounded(String instructions) {
+        int[][] direc = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int direcIndex = 0;
+        int x = 0, y = 0;
+        int n = instructions.length();
+        for (int idx = 0; idx < n; idx++) {
+            char instruction = instructions.charAt(idx);
+            if (instruction == 'G') {
+                x += direc[direcIndex][0];
+                y += direc[direcIndex][1];
+            } else if (instruction == 'L') {
+                direcIndex += 3;
+                direcIndex %= 4;
+            } else {
+                direcIndex++;
+                direcIndex %= 4;
+            }
+        }
+        // 不朝北时，走四圈就到原点了返回true ||  1圈就到原点了
+        return direcIndex != 0 || (x == 0 && y == 0);
+    }
+}
