6222727fc96bff625fedd8b5e348fd5ac6b4b54c
brianwithyou <brianwithyou@163.com>
2023-04-12 10:41:10 +0800

    BST：【99.恢复二分搜索树recover binary search tree】comment

--- src/com/brian/leet99RecoverBinarySearchTree/Solution.java
+++ src/com/brian/leet99RecoverBinarySearchTree/Solution.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 99. 恢复二叉搜索树[medium]
+ * 给你二叉搜索树的根节点 root ，该树中的 恰好 两个节点的值被错误地交换。请在不改变其结构的情况下，恢复这棵树 。
+ *
+ * 输入：root = [1,3,null,null,2]
+ * 输出：[3,1,null,null,2]
+ * 解释：3 不能是 1 的左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+ *
+ * 输入：root = [3,1,4,null,null,2]
+ * 输出：[2,1,4,null,null,3]
+ * 解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。
+ *
+ * 链接：https://leetcode.cn/problems/recover-binary-search-tree
+ *
  * @author : brian
  * @since 0.1
  */
@@ -12,7 +25,20 @@ public class Solution {
     List<Integer> res = new ArrayList<>();
     List<Integer> reversed = new ArrayList<>();
 
-    // 3 2 1
+    /**
+     * time complexity: O(n)
+     * space complexity: O(n)
+     *
+     * 进阶：能否用 O(1)的空间复杂度实现
+     * Follow Up(跟进): A solution using O(n) space is pretty straight-forward. Could you devise(发明) a constant O(1) space solution?
+     * 
+     * Follow-Up Solution:
+     * 使用 Morris中序遍历，迭代方式遍历，
+     * 找到processor前驱节点，将processor的right指向curNode，
+     * if(processor.right != null) 已经遍历过了，然后遍历cur.right,
+     * else 将processor的right指向curNode
+     *
+     */
     public void recoverTree(TreeNode root) {
 
         inorder(root);
