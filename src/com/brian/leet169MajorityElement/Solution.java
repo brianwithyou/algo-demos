d299e5d6c4f0d2831e3e3fed86639793f18deee6
brianwithyou <brianwithyou@163.com>
2023-04-16 18:30:55 +0800

    【数组】【169. 多数元素】【easy】comment

--- src/com/brian/leet169MajorityElement/Solution.java
+++ src/com/brian/leet169MajorityElement/Solution.java
@@ -29,9 +29,10 @@ public class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> count = new HashMap<>();
         int n = nums.length;
+        int threshold = n / 2;
         for (int i = 0; i < n; i++) {
             count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
-            if (count.get(nums[i]) > n / 2) {
+            if (count.get(nums[i]) > threshold) {
                 return nums[i];
             }
         }
