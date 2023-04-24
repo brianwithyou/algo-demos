923a848798f65a263489b981b0d0af1d699628c1
brianwithyou <brianwithyou@163.com>
2023-04-24 22:21:42 +0800

    【字符串】剑指Offer67. 字符串转成整数【自动机】

---
+++ src/com/brian/offer/offer67strToInt/Solution1_autoMachine.java
@@ -0,0 +1,60 @@
+package com.brian.offer.offer67strToInt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution1_autoMachine {
+
+    //    作者：LeetCode-Solution
+    //    链接：https://leetcode.cn/problems/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof/solution/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-sh-epeo/
+    //    来源：力扣（LeetCode）
+    //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    public int strToInt(String str) {
+        Automaton automaton = new Automaton();
+        int length = str.length();
+        for (int i = 0; i < length; ++i) {
+            automaton.get(str.charAt(i));
+        }
+        return (int) (automaton.sign * automaton.ans);
+    }
+}
+
+class Automaton {
+    public int sign = 1;
+    public long ans = 0;
+    private String state = "start";
+    private Map<String, String[]> table = new HashMap<String, String[]>() {{
+        put("start", new String[]{"start", "signed", "in_number", "end"});
+        put("signed", new String[]{"end", "end", "in_number", "end"});
+        put("in_number", new String[]{"end", "end", "in_number", "end"});
+        put("end", new String[]{"end", "end", "end", "end"});
+    }};
+
+    public void get(char c) {
+        state = table.get(state)[get_col(c)];
+        if ("in_number".equals(state)) {
+            ans = ans * 10 + c - '0';
+            ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+        } else if ("signed".equals(state)) {
+            sign = c == '+' ? 1 : -1;
+        }
+    }
+
+    private int get_col(char c) {
+        if (c == ' ') {
+            return 0;
+        }
+        if (c == '+' || c == '-') {
+            return 1;
+        }
+        if (Character.isDigit(c)) {
+            return 2;
+        }
+        return 3;
+    }
+
+}
