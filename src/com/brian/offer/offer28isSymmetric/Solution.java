43f787861d76439533272c001bd0c941e7e93381
brianwithyou <brianwithyou@163.com>
2023-04-28 23:09:12 +0800

    【递归】剑指Offer28. 判断二叉树是否是对称二叉树

---
+++ src/com/brian/offer/offer28isSymmetric/Solution.java
@@ -0,0 +1,58 @@
+package com.brian.offer.offer28isSymmetric;
+
+/**
+ * 剑指 Offer 28. 对称的二叉树
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ *
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+//        if (root == null)
+//            return true;
+//        if (root.left == null && root.right == null)
+//            return true;
+//        if (root.left != null)
+//            return root.left.val == root.right.val;
+
+        return isSymmetric(root, root);
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null)
+            return false;
+        if (left.val != right.val)
+            return false;
+
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+}
