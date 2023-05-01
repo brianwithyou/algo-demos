0383295d3ed0a345734746924a055439d928fc74
brianwithyou <brianwithyou@163.com>
2023-05-01 14:44:10 +0800

    【递归回溯】【剑指 Offer 38. 全排列】

---
+++ src/com/brian/offer/offer38permutation/Solution.java
@@ -0,0 +1,55 @@
+package com.brian.offer.offer38permutation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 剑指 Offer 38. 字符串的排列
+ * 输入一个字符串，打印出该字符串中字符的所有排列。
+ * 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+ *
+ * 输入：s = "abc"
+ * 输出：["abc","acb","bac","bca","cab","cba"]
+ *
+ * 限制：
+ * 1 <= s 的长度 <= 8
+ *
+ * @author : brian
+ * @since 0.1
+ */
+public class Solution {
+    List<String> res = new ArrayList<>();
+    boolean[] used;
+    public String[] permutation(String s) {
+        used = new boolean[s.length()];
+        permutation(s, 0, "");
+        String[] result = new String[res.size()];
+        return res.toArray(result);
+
+    }
+    public void permutation(String s, int index, String temp) {
+        if (index == s.length()) {
+            res.add(temp);
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (!used[i]) {
+                used[i] = true;
+                // code1 当前递归修改temp结果，递归结束需要恢复原状，也可以递归函数中新建一个，参考 code2
+//                temp += s.charAt(i);
+//                permutation(s, index + 1, temp);
+//                temp = temp.substring(0, temp.length() - 1);
+
+                // code2 调用递归时，新建个字符串就不用恢复 temp的值
+                permutation(s, index + 1, temp + s.charAt(i));
+                used[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] abcs = new Solution().permutation("abc");
+        System.out.println(Arrays.toString(abcs));
+    }
+}
